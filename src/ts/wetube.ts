6f556153545a1882c61acb8f4a35cc22da2b9990
Adam Nelson <dev@sector91.com>
2014-11-25 18:40:25 -0500

    Added basic YouTube video loading code.

--- src/ts/wetube.ts
+++ src/ts/wetube.ts
@@ -1,5 +1,17 @@
+/// <reference path="typings/jquery.d.ts" />
+/// <reference path="typings/swfobject.d.ts" />
+/// <reference path="typings/youtube.d.ts" />
 
-function foo(): string {
-  return "Hello, world!"
+function initYouTube(videoId: string): void {
+    var params = { allowScriptAccess: 'always', allowFullScreen: 'true' };
+    var attrs = { id: 'ytapiplayer' };
+    swfobject.embedSWF("http://www.youtube.com/v/" + videoId +
+        "?hl=en_US&rel=0&hd=1&border=0&version=3&fs=1&autoplay=1&autohide=0&enablejsapi=1&playerapiid=ytapiplayer",
+        'ytapiplayer', '640', '360', '8', null, null, params, attrs);
 }
 
+function onYouTubePlayerReady(id) {
+    console.log("YouTube player is ready.");
+}
+
+$(document).ready(() => initYouTube("dQw4w9WgXcQ")); // Never gonna give you up...
